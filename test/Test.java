b10e9e4453f720a4c202b8558fa960e546d3dbc5
moritz <mmairinger2@student.tgm.ac.at>
2023-04-30 21:18:54 +0200

    Comments complete

--- test/Test.java
+++ test/Test.java
@@ -1,25 +1,42 @@
 public class Test {
-    // Schreibe eine Testklasse für deinen Stack, in dem du sowohl einen Zahlenstack testest und dann einen Textstack. Versuche dir dabei sinnvolle Testszenarien zu überlegen, so dass die Funktionsweise deines Stacks gut gezeigt wird. Dokumentiere sowohl deine Klasse mit Dokumentationskommentaren, als auch die Testklasse mit Zeilenkommentaren.
     static public void main(String[] args) throws Exception {
+        // Creates a new Stack object with a generic Integer
         Stack<Integer> s = new Stack();
+        // pushes the value 1 into the stack
         s.push(1);
+        // pushes the value 2 into the stack
         s.push(2);
+        // pushes the value 3 into the stack
         s.push(3);
+        // prints the stack
         System.out.println(s.list());
+        // pops the last element from the stack
         System.out.println(s.pop());
+        // prints the stack
         System.out.println(s.list());
+        // prints the last element from the stack
         System.out.println(s.peek());
+        // prints the stack
         System.out.println(s.list());
 
 
+        // Creates a new Stack object with a generic String
         Stack<String> s2 = new Stack();
+        // pushes the value "Hallo" into the stack
         s2.push("Hallo");
+        // pushes the value "Welt" into the stack
         s2.push("Welt");
+        // pushes the value "!" into the stack
         s2.push("!");
+        // prints the stack
         System.out.println(s2.list());
+        // pops the last element from the stack
         System.out.println(s2.pop());
+        // prints the stack
         System.out.println(s2.list());
+        // prints the last element from the stack
         System.out.println(s2.peek());
+        // prints the stack
         System.out.println(s.list());
 
 
